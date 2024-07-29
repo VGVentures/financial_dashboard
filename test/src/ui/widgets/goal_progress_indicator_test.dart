a95b7f4a9630d91fa09f546a5f53d54f465936dc
Joanna May <joannamadisonmay@gmail.com>
2024-07-29 14:33:40 -0500

    feat: make progress visualizer functional

--- test/src/ui/widgets/goal_progress_indicator_test.dart
+++ test/src/ui/widgets/goal_progress_indicator_test.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
+import '../../../helpers/pump_experience.dart';
+
 class MockColorScheme extends Mock implements ColorScheme {
   @override
   String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
@@ -12,30 +14,59 @@ class MockColorScheme extends Mock implements ColorScheme {
 
 void main() {
   group('CircleProgressPainter', () {
+    group('$GoalProgressIndicator', () {
+      testWidgets('renders without gradient', (tester) async {
+        await tester.pumpExperience(const GoalProgressIndicator(value: 1));
+        await tester.pumpAndSettle();
+        expect(find.byType(GoalProgressIndicator), findsOneWidget);
+      });
+
+      testWidgets('renders with gradient', (tester) async {
+        await tester.pumpExperience(
+          const GoalProgressIndicator(
+            isGradient: true,
+          ),
+        );
+        await tester.pumpAndSettle();
+        expect(find.byType(GoalProgressIndicator), findsOneWidget);
+      });
+    });
+
     group('shouldRepaint', () {
       test('returns false if nothing changes', () {
         final colorScheme = MockColorScheme();
+        when(() => colorScheme.primary).thenReturn(Colors.red);
 
         final newDelegate = CircleProgressPainter(
           colorScheme: colorScheme,
           isGradient: false,
+          value: 1,
         );
         final oldDelegate = CircleProgressPainter(
           colorScheme: colorScheme,
           isGradient: false,
+          value: 1,
         );
 
         expect(newDelegate.shouldRepaint(oldDelegate), isFalse);
       });
 
       test('returns true when colorScheme changes', () {
+        final colorScheme = MockColorScheme();
+        when(() => colorScheme.primary).thenReturn(Colors.red);
+
         final newDelegate = CircleProgressPainter(
-          colorScheme: MockColorScheme(),
+          colorScheme: colorScheme,
           isGradient: false,
+          value: 1,
         );
+
+        final colorScheme2 = MockColorScheme();
+        when(() => colorScheme2.primary).thenReturn(Colors.blue);
         final oldDelegate = CircleProgressPainter(
-          colorScheme: MockColorScheme(),
+          colorScheme: colorScheme2,
           isGradient: false,
+          value: 1,
         );
 
         expect(newDelegate.shouldRepaint(oldDelegate), isTrue);
@@ -43,14 +74,18 @@ void main() {
 
       test('returns true when isGradient changes', () {
         final colorScheme = MockColorScheme();
+        when(() => colorScheme.primary).thenReturn(Colors.red);
 
         final newDelegate = CircleProgressPainter(
           colorScheme: colorScheme,
           isGradient: false,
+          value: 1,
         );
+
         final oldDelegate = CircleProgressPainter(
           colorScheme: colorScheme,
           isGradient: true,
+          value: 1,
         );
 
         expect(newDelegate.shouldRepaint(oldDelegate), isTrue);
