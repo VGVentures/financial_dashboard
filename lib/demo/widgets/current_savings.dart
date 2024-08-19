b5ffeb09d910279afc2e5ec4876e1a4c4886f23b
Hugo Walbecq <hugo@verygood.ventures>
2024-08-19 09:15:48 +0200

    feat: animate current savings

--- lib/demo/widgets/current_savings.dart
+++ lib/demo/widgets/current_savings.dart
@@ -4,10 +4,34 @@ import 'package:financial_dashboard/ui/ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CurrentSavings extends StatelessWidget {
+class CurrentSavings extends StatefulWidget {
   const CurrentSavings({
     super.key,
   });
+
+  @override
+  State<CurrentSavings> createState() => _CurrentSavingsState();
+}
+
+class _CurrentSavingsState extends State<CurrentSavings>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +41,24 @@ class CurrentSavings extends StatelessWidget {
       (FinancialDataBloc bloc) => bloc.state.currentSavings,
     );
 
+    _animation = Tween<double>(begin: 0, end: currentSavings).animate(
+      _controller,
+    );
+
+    _controller.forward();
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          currentSavings.toCurrencyWithoutDecimal(),
-          style: textTheme.displayMedium,
+        AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            final value = _animation.value;
+            return Text(
+              value.toCurrencyWithoutDecimal(),
+              style: textTheme.displayMedium,
+            );
+          },
         ),
         const SizedBox(height: AppSpacing.xs),
         Text(
