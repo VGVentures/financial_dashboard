9ac189ecaed43c22229f3987de3ccca77ed86f2d
Jaime <jaimesanchez8@gmail.com>
2024-08-21 13:11:22 +0200

    feat: animate monthly goal

--- lib/demo/widgets/monthly_goal.dart
+++ lib/demo/widgets/monthly_goal.dart
@@ -4,10 +4,42 @@ import 'package:financial_dashboard/ui/ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MonthlyGoal extends StatelessWidget {
+class MonthlyGoal extends StatefulWidget {
   const MonthlyGoal({
     super.key,
   });
+
+  @override
+  State<MonthlyGoal> createState() => _MonthlyGoalState();
+}
+
+class _MonthlyGoalState extends State<MonthlyGoal>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    final state = context.read<FinancialDataBloc>().state;
+    _animation = Tween<double>(
+      begin: 0,
+      end: state.monthlySpendingLimitGoal,
+    ).animate(_controller);
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,34 +47,51 @@ class MonthlyGoal extends StatelessWidget {
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
     final colorScheme = theme.colorScheme;
-    final monthlySpendingLimitGoal = context.select(
-      (FinancialDataBloc bloc) => bloc.state.monthlySpendingLimitGoal,
-    );
 
-    return Card(
-      elevation: 0,
-      color: colorScheme.inversePrimary.withOpacity(0.6),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: AppSpacing.lg,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FittedBox(
-              fit: BoxFit.scaleDown,
-              child: DefaultTextStyle(
-                style: textTheme.displaySmall!,
-                child: Text(monthlySpendingLimitGoal.toCurrencyWithDecimals()),
+    return BlocListener<FinancialDataBloc, FinancialDataState>(
+      listenWhen: (previous, current) =>
+          previous.monthlySpendingLimitGoal != current.monthlySpendingLimitGoal,
+      listener: (context, state) {
+        _animation = Tween<double>(
+          begin: 0,
+          end: state.monthlySpendingLimitGoal,
+        ).animate(_controller);
+
+        _controller
+          ..reset()
+          ..forward();
+      },
+      child: Card(
+        elevation: 0,
+        color: colorScheme.inversePrimary.withOpacity(0.6),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: AppSpacing.lg,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FittedBox(
+                fit: BoxFit.scaleDown,
+                child: DefaultTextStyle(
+                  style: textTheme.displaySmall!,
+                  child: AnimatedBuilder(
+                    animation: _animation,
+                    builder: (context, child) {
+                      final value = _animation.value;
+                      return Text(value.toCurrencyWithDecimals());
+                    },
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: AppSpacing.xxs),
-            Text(
-              l10n.monthlySpendingLimitGoal,
-              style: textTheme.labelMedium,
-            ),
-          ],
+              const SizedBox(height: AppSpacing.xxs),
+              Text(
+                l10n.monthlySpendingLimitGoal,
+                style: textTheme.labelMedium,
+              ),
+            ],
+          ),
         ),
       ),
     );
